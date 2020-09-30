7a89359d221936b60d69c81b01e4632c9f86f188
htrom14 <hjjames@wpi.edu>
2020-09-30 12:55:06 -0400

    Setup Main

---
+++ Main.py
@@ -0,0 +1,16 @@
+
+
+
+def main():
+    """
+    Entry point for this program.
+
+    """ 
+    # listenForTurn()
+    # getMove()
+    # writeMove()
+    # checkGameOver()
+    return None
+
+if __name__ == "__main__": 
+    main()
