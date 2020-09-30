1e2051319bfa8fe34420e69512725cc768244df6
htrom14 <hjjames@wpi.edu>
2020-09-30 12:58:45 -0400

    Cpp version

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+
+#include <sys/types.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    // listenForTurn()
+    // getMove()
+    // writeMove()
+    // checkGameOver()
+    return 0;
+}
