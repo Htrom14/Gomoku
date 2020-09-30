258d9fd365ecfbb3666714e6d280797602a4ad25
htrom14 <hjjames@wpi.edu>
2020-09-30 15:34:42 -0400

    Merged functionality

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,18 +13,28 @@ using namespace std;
 
 FileHandler * fileHandler;
 int board[15][15] = {};
+int madeMoveRow;
+int madeMoveCol;
 
 int main(int argc, char *argv[])
 {
+    madeMoveRow = 0;
+    madeMoveCol = 0;
     while(true){
 		if(fileHandler->listenForTurn()){
-            cout << "My turn" << endl;
+            // cout << "My turn" << endl;
             if(fileHandler->checkGameOver()){
                 break;
             }
             // getMove()
             std::string lastMove = fileHandler->getLastMove();
             cout << lastMove << endl;
+            translateMoveToCoords(lastMove, &madeMoveRow,& madeMoveCol);
+            cout << board[14][0] << endl;
+            cout<< "MoveRow: " << madeMoveRow << endl;
+            cout << "MoveCol: " << madeMoveCol << endl;
+            makeMoveOnBoard(board, madeMoveRow, madeMoveCol, 2);
+            cout << board[14][0] << endl;
             break;
             // writeMove()
 		}else{
